61b4b2936966e14404642c7028407d9aa057ed69
Oliwier Wozniak <owozniakp@gmail.com>
2026-04-06 16:00:29 +0200

    Now it compiles (stupid shit)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -33,6 +33,16 @@
 #include <stdio.h>
 #include <string.h>
 #include "arm_math.h" // Niezbędne dla float32_t i funkcji FFT
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <uxr/client/transport.h>
+#include <rmw_microxrcedds_c/config.h>
+#include <rmw_microros/rmw_microros.h>
+
+#include <std_msgs/msg/int32.h>
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -55,18 +65,27 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-// Bufory surowe dla DMA (rozmiar * 2 dla obsługi Half Transfer i Complete Transfer)
-uint16_t Raw_ADC1[CH_PER_ADC * SAMPLES * 2];
-uint16_t Raw_ADC3[CH_PER_ADC * SAMPLES * 2];
-uint16_t Raw_ADC4[CH_PER_ADC * SAMPLES * 2];
+/* USER CODE BEGIN PV */
+osThreadId_t microrosTaskHandle;
+const osThreadAttr_t microrosTask_attributes = {
+  .name = "microrosTask",
+  .priority = (osPriority_t) osPriorityNormal,
+  .stack_size = 3000 * 4 // micro-ROS potrzebuje sporo stosu!
+};
 
-// Bufor po rozdzieleniu kanałów, gotowy do FFT
-float32_t FFT_Input_Buffer[TOTAL_CHANNELS][SAMPLES];
+osThreadId_t adcTaskHandle;
+const osThreadAttr_t adcTask_attributes = {
+  .name = "adcTask",
+  .priority = (osPriority_t) osPriorityAboveNormal, // Wyższy priorytet dla przetwarzania danych
+  .stack_size = 1000 * 4
+};
 
-// Flaga informująca o gotowości danych (volatile, bo zmieniana w przerwaniu)
-volatile uint8_t data_ready_flag = 0;
-
-uint8_t UartMessage[64];
+// Obiekty micro-ROS
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -76,6 +95,25 @@ void MX_FREERTOS_Init(void);
 void ADC_SetActiveChannel(ADC_HandleTypeDef *hadc, uint32_t AdcChannel);
 void UART4_Print(uint8_t* Message);
 
+void StartMicroRosTask(void *argument);
+void StartADCTask(void *argument);
+
+/* USER CODE BEGIN PFP */
+void StartMicroRosTask(void *argument);
+void StartADCTask(void *argument);
+
+// Deklaracje funkcji transportowych micro-ROS
+bool cubemx_transport_open(struct uxrCustomTransport * transport);
+bool cubemx_transport_close(struct uxrCustomTransport * transport);
+size_t cubemx_transport_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err);
+size_t cubemx_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err);
+
+// Deklaracje allocatorów
+void * microros_allocate(size_t size, void * state);
+void microros_deallocate(void * pointer, void * state);
+void * microros_reallocate(void * pointer, size_t size, void * state);
+void * microros_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state);
+/* USER CODE END PFP */
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -134,16 +172,6 @@ int main(void)
   MX_USART3_UART_Init();
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-    HAL_TIM_Base_Start(&htim3);
-
-    HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
-    HAL_ADCEx_Calibration_Start(&hadc3, ADC_SINGLE_ENDED);
-    HAL_ADCEx_Calibration_Start(&hadc4, ADC_SINGLE_ENDED);
-
-    // Rozmiar w HAL_ADC_Start_DMA podajemy jako całkowitą liczbę elementów w tablicy
-    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)Raw_ADC1, CH_PER_ADC * SAMPLES * 2);
-    HAL_ADC_Start_DMA(&hadc3, (uint32_t*)Raw_ADC3, CH_PER_ADC * SAMPLES * 2);
-    HAL_ADC_Start_DMA(&hadc4, (uint32_t*)Raw_ADC4, CH_PER_ADC * SAMPLES * 2);
 
     UART4_Print((uint8_t*)"System Ready\n");
   /* USER CODE END 2 */
@@ -151,6 +179,12 @@ int main(void)
   /* Init scheduler */
   osKernelInitialize();  /* Call init function for freertos objects (in cmsis_os2.c) */
   MX_FREERTOS_Init();
+
+  /* USER CODE BEGIN RTOS_THREADS */
+    // Tworzymy zadania
+    microrosTaskHandle = osThreadNew(StartMicroRosTask, NULL, &microrosTask_attributes);
+    adcTaskHandle = osThreadNew(StartADCTask, NULL, &adcTask_attributes);
+  /* USER CODE END RTOS_THREADS */
 
   /* Start scheduler */
   osKernelStart();
@@ -161,39 +195,6 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-
-	  if (data_ready_flag > 0) {
-		  HAL_GPIO_TogglePin(GPIOB, 3);
-	            // Jeśli flaga == 1, czytamy pierwszą połowę (offset 0)
-	            // Jeśli flaga == 2, czytamy drugą połowę (offset po SAMPLES * kanały)
-	            uint32_t offset = (data_ready_flag == 1) ? 0 : (CH_PER_ADC * SAMPLES);
-
-	            for (int i = 0; i < SAMPLES; i++) {
-	                // Rozdzielanie danych (De-interleaving)
-	                // ADC1: Diody 0, 1, 2
-	                FFT_Input_Buffer[0][i] = (float32_t)Raw_ADC1[offset + i*CH_PER_ADC + 0];
-	                FFT_Input_Buffer[1][i] = (float32_t)Raw_ADC1[offset + i*CH_PER_ADC + 1];
-	                FFT_Input_Buffer[2][i] = (float32_t)Raw_ADC1[offset + i*CH_PER_ADC + 2];
-
-	                // ADC3: Diody 3, 4, 5
-	                FFT_Input_Buffer[3][i] = (float32_t)Raw_ADC3[offset + i*CH_PER_ADC + 0];
-	                FFT_Input_Buffer[4][i] = (float32_t)Raw_ADC3[offset + i*CH_PER_ADC + 1];
-	                FFT_Input_Buffer[5][i] = (float32_t)Raw_ADC3[offset + i*CH_PER_ADC + 2];
-
-	                // ADC4: Diody 6, 7 i Reference
-	                FFT_Input_Buffer[6][i] = (float32_t)Raw_ADC4[offset + i*CH_PER_ADC + 0];
-	                FFT_Input_Buffer[7][i] = (float32_t)Raw_ADC4[offset + i*CH_PER_ADC + 1];
-	                FFT_Input_Buffer[8][i] = (float32_t)Raw_ADC4[offset + i*CH_PER_ADC + 2];
-	            }
-
-	            // RESET FLAGI - ważne!
-	            data_ready_flag = 0;
-
-	            // Opcjonalny test: wyświetl wartość pierwszej próbki pierwszej diody
-	            sprintf((char*)UartMessage, "Dioda0[0]: %.2f\r\n", FFT_Input_Buffer[1][0]);
-	            UART4_Print(UartMessage);
-	        }
-
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -217,11 +218,12 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV2;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
   RCC_OscInitStruct.PLL.PLLN = 85;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
@@ -248,20 +250,66 @@ void SystemClock_Config(void)
 
 /* USER CODE BEGIN 4 */
 
-// Callback wywoływany w połowie zapełnienia bufora
-void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc) {
-    if(hadc->Instance == ADC1) data_ready_flag = 1;
+// --- ZADANIE PRZETWARZANIA ADC ---
+void StartADCTask(void *argument) {
+
 }
 
-// Callback wywoływany przy pełnym buforze
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
-    if(hadc->Instance == ADC1) data_ready_flag = 2;
+// --- ZADANIE MICRO-ROS ---
+void StartMicroRosTask(void *argument) {
+	rmw_uros_set_custom_transport(
+	        true,
+	        (void *) &huart1,
+	        cubemx_transport_open,
+	        cubemx_transport_close,
+	        cubemx_transport_write,
+	        cubemx_transport_read
+	    );
+
+    allocator = rcl_get_default_allocator();
+
+    // 2. Inicjalizacja wsparcia i węzła
+    rclc_support_init(&support, 0, NULL, &allocator);
+    rclc_node_init_default(&node, "uMule_Receiver_Node", "", &support);
+
+    // 3. Inicjalizacja publisherea
+    rclc_publisher_init_default(
+        &publisher, &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+        "sensor_status"
+    );
+
+    msg.data = 0;
+
+    for(;;) {
+        // Publikacja licznika
+        rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
+
+        if (ret == RCL_RET_OK) {
+            msg.data++; // Inkrementacja "statusu"
+        }
+
+        osDelay(1000); // Czekaj dokładnie 1 sekundę
+    }
 }
+
 
 void UART4_Print(uint8_t* Message)
 {
 	HAL_UART_Transmit(&huart4, Message, strlen((char*)Message), 100);
 }
+
+/* USER CODE BEGIN 4 */
+bool cubemx_transport_open(struct uxrCustomTransport * transport);
+bool cubemx_transport_close(struct uxrCustomTransport * transport);
+size_t cubemx_transport_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err);
+size_t cubemx_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err);
+
+void * microros_allocate(size_t size, void * state);
+void microros_deallocate(void * pointer, void * state);
+void * microros_reallocate(void * pointer, size_t size, void * state);
+void * microros_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state);
+
 /* USER CODE END 4 */
 
 /**
